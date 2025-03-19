e426d0fc9e710f4e757e1c8d82a8fca6d6242e4c
TheBlueDrara <soony0737@gmail.com>
2025-03-19 18:05:06 +0200

    added a new task, and modifed so the task will be part of the file inside

---
+++ python_task_02.py
@@ -0,0 +1,19 @@
+# Page 45
+
+# TASK
+# Create python script file where you:
+# create variable for wifi/eth/network that you are connected to, and put in it that name.
+# create variable for your laptops hostname and save the name in it.
+# concatenate the variable above with new variable named my_con .
+# print() the variable
+# add id (or any number) of your connection to my_con variable and re-print it. 
+
+
+i_face = "Bos was here"
+print(i_face)
+print(i_face[3:8:])
+print(i_face[10])
+print(i_face[0].isupper())
+
+
+
---
+++ python_task_03.py
@@ -0,0 +1,20 @@
+# page 49
+
+
+# TASK
+# Create a python script
+# request from user to input some data twice
+# on the first input add back slash as part of input
+# print the input immediately
+# on the second input add double quote as part input
+# seek for double quote and print its position
+
+
+
+num1 = input("User Input1: ")
+num1_modifed = num1 + "\\"
+print(num1_modifed)
+
+num2 = input("User Input2: ")
+num2_modifed = num2 + "\""
+print(num2_modifed)
